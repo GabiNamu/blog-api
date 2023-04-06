6aab7763a15a43f2536932e1861532edcb98f7e6
Gabriela Muniz <gabrielanamu@live.com>
2023-04-06 18:45:32 -0300

    feat: add req11 - create PostCategory model

---
+++ src/models/PostCategory.js
@@ -0,0 +1,28 @@
+module.exports = (sequelize, DataTypes) => {
+    const PostCategory = sequelize.define('PostCategory', {
+        postId: { type: DataTypes.INTEGER, foreignKey: true, primaryKey: true},
+        categoryId: { type: DataTypes.INTEGER, foreignKey: true, primaryKey: true},
+    },  
+    {
+        timestamps: false,
+        tableName: 'posts_categories',
+        underscored: true,
+    });
+
+    PostCategory.associate = (models) => {
+        models.Category.belongsToMany(models.BlogPost, {
+          as: 'posts',
+          through: PostCategory,
+          foreignKey: 'categoryId',
+          otherKey: 'postId',
+        });
+        models.BlogPost.belongsToMany(models.Category, {
+          as: 'categories',
+          through: PostCategory,
+          foreignKey: 'postId',
+          otherKey: 'categoryId',
+        });
+      };
+  
+    return PostCategory;
+  };
