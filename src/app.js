859c4038e13d67124ecb46233571a1fa648a292f
Gabriela Muniz <gabrielanamu@live.com>
2023-04-06 14:39:25 -0300

    start project

--- src/app.js
+++ src/app.js
@@ -13,6 +13,6 @@ app.use(express.json());
 
 // ...
 
-// É importante exportar a constante `app`,
+// É importante exportar a constante `app`, oi
 // para que possa ser utilizada pelo arquivo `src/server.js`
 module.exports = app;
